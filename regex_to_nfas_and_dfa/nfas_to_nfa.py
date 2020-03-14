4b4f907c76aae0951f996222f2b365902bcd8fa1
TrungLuong1194 <trungluong1194@gmail.com>
2020-03-14 23:47:11 +0300

    update

--- regex_to_nfas_and_dfa/nfas_to_nfa.py
+++ regex_to_nfas_and_dfa/nfas_to_nfa.py
@@ -21,31 +21,21 @@ class NFAs_To_NFA:
 				vertex_to = []
 				
 				listClosures = sorted(self.epsilonClosures.operands[indexStart].getClosure())
-				# print('listClosures' + str(listClosures))
 
 				listClosuresIn = []
 				for indexClosure in range(len(listClosures)):
-					# print(listClosures[indexClosure])
 					if self.nfa.get_vertex_to(listClosures[indexClosure], trans_symbol) != 'empty':
 						listClosuresIn.append(self.nfa.get_vertex_to(listClosures[indexClosure], trans_symbol))
-
-				# print('listClosuresIn' + str(listClosuresIn))
 
 				listTmp = []
 				if len(listClosuresIn) > 0:
 					for i in range(len(listClosuresIn)):
 						listTmp.append(sorted(self.epsilonClosures.get_epsilon(listClosuresIn[i])))
 
-				# print('listTmp' + str(listTmp))
-
 				for i in range(len(listTmp)):
 					function.unionList(vertex_to, listTmp[i])
 
-				# print('vertex_to' + str(vertex_to))
-
 				tmp.set_transition(vertex_from, vertex_to, trans_symbol)
-
-				# print('get_final_state' + str(self.nfa.get_final_state()))
 
 				for i in range(len(vertex_to)):
 					if self.nfa.get_final_state() in vertex_to and vertex_to not in final_state:
