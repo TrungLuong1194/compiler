0f9c476fadeed3d4e939c470d7c2fefa9b4da66b
TrungLuong1194 <trungluong1194@gmail.com>
2020-06-11 02:50:16 +0300

    update

---
+++ lexer_analysis/input.py
@@ -0,0 +1,10 @@
+x = 5
+
+if x >= 5:
+    y = 0
+
+for i in range(5):
+    x = x + 1
+    print(x)
+
+0ab = 5
--- lexer_analysis/main.py
+++ lexer_analysis/main.py
@@ -1,37 +1,66 @@
-# from lexer_analysis.regex_to_dfa import RegexToDFA
-# import string
-#
-# # Digits
-# digit = '(0|1|2|3|4|5|6|7|8|9)'
-#
-# digit_token = RegexToDFA(digit)
-# # digit_token.transform()
-#
-# trans = digit_token.transform()
-# trans.display()
-#
-#
-# # Letters
-# letter = string.ascii_letters
-# letter_list = '('
-#
-# for i in letter:
-#     letter_list += i
-#     if i != 'Z':
-#         letter_list += '|'
-#
-# letter_list = letter_list + ')'
-#
-# letter_token = RegexToDFA(letter_list)
-# letter_token.transform()
-#
-# # Operators
-# operator = '(<|>|=|(<.=)|(>.=)|(=.=))'
-#
-# operator_token = RegexToDFA(operator)
-# operator_token.transform()
+import lexer_analysis.check_word as check
 
-from lexer_analysis.check_lexeme import CheckLexeme
+file = open('input.py', 'r')
 
-check_lexeme = CheckLexeme('1234')
-check_lexeme.isdigit()
+word_dict = {}
+line_index = 0
+
+for line in file:
+    line_index += 1
+    word_list = []
+
+    if line != '\n':
+        beginPtr = None
+
+        for i in range(len(line)):
+            if line[i] != ' ':
+                beginPtr = i
+                break
+
+        lastPtr = beginPtr
+
+        while lastPtr < len(line):
+            if check.isDelimiter(line[lastPtr]):
+
+                if beginPtr != lastPtr:
+                    word_list.append(line[beginPtr:lastPtr])
+                if line[lastPtr] != ' ' and line[lastPtr] != '\n':
+                    word_list.append(line[lastPtr])
+
+                beginPtr = lastPtr + 1
+
+            lastPtr += 1
+
+    word_dict[line_index] = word_list
+
+tokens = {}
+index_identifier = 0
+index_digit = 0
+
+for key, value in word_dict.items():
+    token_list = []
+    for v in value:
+        if check.isKeyword(v):
+            token_list.append('KEYWORD')
+        elif check.isDelimiter(v):
+            token_list.append(v)
+        elif check.isOperator(v):
+            token_list.append(v)
+        elif check.isDigit(v):
+            token_list.append('DIGIT' + str(index_digit))
+            index_digit += 1
+        elif check.isIdentifier(v):
+            token_list.append('ID' + str(index_identifier))
+            index_identifier += 1
+        else:
+            token_list.append('error')
+
+    tokens[key] = token_list
+
+for key, value in tokens.items():
+    print(value)
+
+
+
+
+
